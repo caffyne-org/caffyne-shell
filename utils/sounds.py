f52df174f24d6ca2e228adb7b8af5036e62954e5
coffeeisangry <caffyne@proton.me>
2026-06-06 22:04:10 +0100

    Change sound backend to just pipewire/pulseaudio

--- utils/sounds.py
+++ utils/sounds.py
@@ -1,24 +1,21 @@
-import ctypes
 import shutil
+from fabric.utils import exec_shell_command_async
 from fabric.utils import get_relative_path
 
-def get_sound_path(name: str) -> str:
-    return get_relative_path("../sounds/" + name + ".wav")
+def _detect_player() -> str | None:
+    for player in ("pw-play", "paplay"):
+        if shutil.which(player):
+            return player
+    return None
 
-try:
-    _ca = ctypes.CDLL("libcanberra.so.0")
-    _ctx = ctypes.c_void_p()
-    _ca.ca_context_create(ctypes.byref(_ctx))
-    _BACKEND = "canberra"
-except OSError:
-    _BACKEND = next((p for p in ("pw-play", "paplay") if shutil.which(p)), None)
+def get_sound_path(svg_name):
+    return get_relative_path("../sounds/" + svg_name + ".wav")
+
+_PLAYER = _detect_player()
 
 def play_sound(name: str) -> None:
-    path = get_sound_path(name)
-    if _BACKEND == "canberra":
-        _ca.ca_context_play(_ctx, 0, b"media.filename", path.encode(), None)
-    elif _BACKEND:
-        from fabric.utils import exec_shell_command_async
-        exec_shell_command_async(f"{_BACKEND} {path}")
-    else:
-        print("[play_sound] No audio backend found (tried libcanberra, pw-play, paplay)")
+    print("play")
+    if _PLAYER is None:
+        print("[play_sound] No audio player found (tried pw-play, paplay)")
+        return
+    exec_shell_command_async(f"{_PLAYER} {get_sound_path(name)}")
